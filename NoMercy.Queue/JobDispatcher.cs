164fdd70e0ab59ded44cb3dd811d4195aa292e86
Stoney Eagle <45034970+StoneyEagle@users.noreply.github.com>
2024-08-20 21:41:35 +0200

    wip: media processing logic refactor

---
+++ NoMercy.Queue/JobDispatcher.cs
@@ -0,0 +1,36 @@
+using NoMercy.Database;
+using NoMercy.Database.Models;
+using NoMercy.NmSystem;
+using Serilog.Events;
+
+namespace NoMercy.Queue;
+
+public interface IShouldQueue
+{
+    Task Handle();
+}
+
+public class JobDispatcher
+{
+    private static readonly JobQueue Queue = new(new QueueContext());
+
+    public static void Dispatch(IShouldQueue job, string onQueue = "default", int priority = 0)
+    {
+        QueueJob jobData = new()
+        {
+            Queue = onQueue,
+            Payload = SerializationHelper.Serialize(job),
+            AvailableAt = DateTime.UtcNow,
+            Priority = priority
+        };
+
+        try
+        {
+            Queue.Enqueue(jobData);
+        }
+        catch (Exception e)
+        {
+            Logger.Queue(e, LogEventLevel.Error);
+        }
+    }
+}
