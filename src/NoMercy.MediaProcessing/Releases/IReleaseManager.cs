7fc50b45f6f82502ca1d50cefb2f5cfb342cfc5c
AndreasSasDev <directive.athena@gmail.com>
2024-09-17 17:42:49 +0200

    Fix: Corrected forgotten files

---
+++ src/NoMercy.MediaProcessing/Releases/IReleaseManager.cs
@@ -0,0 +1,12 @@
+using NoMercy.Database.Models;
+using NoMercy.MediaProcessing.Images;
+using NoMercy.NmSystem;
+using NoMercy.Providers.MusicBrainz.Models;
+
+namespace NoMercy.MediaProcessing.Releases;
+
+public interface IReleaseManager
+{
+    public Task<(MusicBrainzReleaseAppends? releaseAppends, CoverArtImageManagerManager.CoverPalette? coverPalette)> Add(Guid id, Library albumLibrary, Folder libraryFolder,
+        MediaFolder mediaFolder);
+}
---
+++ src/NoMercy.MediaProcessing/Releases/ReleaseRepository.cs
@@ -0,0 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+using NoMercy.Database;
+using NoMercy.Database.Models;
+
+namespace NoMercy.MediaProcessing.Releases;
+
+public class ReleaseRepository(MediaContext context) : IReleaseRepository
+{
+    public Task Store(Album release)
+    {
+        return context.Albums.Upsert(release)
+            .On(e => new { e.Id })
+            .WhenMatched((s, i) => new Album
+            {
+                UpdatedAt = DateTime.UtcNow,
+                Id = i.Id,
+                Name = i.Name,
+                Disambiguation = i.Disambiguation,
+                Description = i.Description,
+                Year = i.Year,
+                Country = i.Country,
+                Tracks = i.Tracks,
+
+                LibraryId = i.LibraryId,
+                Folder = i.Folder,
+                FolderId = i.FolderId,
+                HostFolder = i.HostFolder
+            })
+            .RunAsync();
+    }
+
+    public Task LinkToReleaseGroup(AlbumReleaseGroup albumReleaseGroup)
+    {
+        return context.AlbumReleaseGroup.Upsert(albumReleaseGroup)
+            .On(e => new { e.AlbumId, e.ReleaseGroupId })
+            .WhenMatched((s, i) => new AlbumReleaseGroup
+            {
+                AlbumId = i.AlbumId,
+                ReleaseGroupId = i.ReleaseGroupId
+            })
+            .RunAsync();
+    }
+
+    public Task LinkToLibrary(AlbumLibrary albumLibrary)
+    {
+        return context.AlbumLibrary.Upsert(albumLibrary)
+            .On(e => new { e.AlbumId, e.LibraryId })
+            .WhenMatched((s, i) => new AlbumLibrary
+            {
+                AlbumId = i.AlbumId,
+                LibraryId = i.LibraryId
+            })
+            .RunAsync();
+    }
+}
