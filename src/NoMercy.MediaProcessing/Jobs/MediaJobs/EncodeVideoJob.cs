76cf06c66b458071fd1cdc3b20f621927b97198b
Stoney Eagle <45034970+StoneyEagle@users.noreply.github.com>
2024-09-27 14:48:44 +0200

    fml: thanks ducky!

--- src/NoMercy.MediaProcessing/Jobs/MediaJobs/EncodeVideoJob.cs
+++ src/NoMercy.MediaProcessing/Jobs/MediaJobs/EncodeVideoJob.cs
@@ -11,7 +11,6 @@ using NoMercy.Encoder.Format.Rules;
 using NoMercy.Encoder.Format.Subtitle;
 using NoMercy.Encoder.Format.Video;
 using NoMercy.NmSystem;
-using Vtt = NoMercy.Encoder.Format.Subtitle.Vtt;
 
 namespace NoMercy.MediaProcessing.Jobs.MediaJobs;
 
@@ -27,33 +26,33 @@ public class EncodeVideoJob : AbstractEncoderJob
 
         LibraryRepository libraryRepository = new(context);
         LibraryManager libraryManager = new(libraryRepository, jobDispatcher);
-        
+
         Folder? folder = await libraryRepository.GetLibraryFolder(FolderId);
         if (folder is null) return;
-        
+
         List<EncoderProfile> profiles = folder.EncoderProfileFolder
             .Select(e => e.EncoderProfile)
             .ToList();
-        
+
         if (!profiles.Any()) return;
-        
+
         var movie = folder.FolderLibraries.Any(x => x.Library.Type == "movie")
             ? await context.Movies
-                .FirstOrDefaultAsync(x => x.Id == Id) 
+                .FirstOrDefaultAsync(x => x.Id == Id)
             : null;
-        
+
         var episode = folder.FolderLibraries.Any(x => x.Library.Type == "tv" || x.Library.Type == "anime")
             ? await context.Episodes
                 .Include(x => x.Tv)
                 .FirstOrDefaultAsync(x => x.Id == Id)
-            : null; 
-        
+            : null;
+
         if (movie is null && episode is null) return;
-        
-        var folderName = (movie?.CreateFolderName().Replace("/", "") 
+
+        var folderName = (movie?.CreateFolderName().Replace("/", "")
                           ?? episode!.Tv.CreateFolderName().Replace("/", "") + episode.CreateFolderName())
             .Replace("/", Path.DirectorySeparatorChar.ToString());
-        
+
         var title = movie?.CreateTitle() ?? episode!.CreateTitle();
         var fileName = movie?.CreateFileName() ?? episode!.CreateFileName();
         var basePath = Path.Combine(folder.Path, folderName);
@@ -67,30 +66,30 @@ public class EncodeVideoJob : AbstractEncoderJob
                 "Hls" => new Hls().SetHlsFlags("independent_segments"),
                 _ => new Hls().SetHlsFlags("independent_segments")
             };
-            
-            BuildVideoStreams(profile, container);
-            BuildAudioStreams(profile, container);
-            BuildSubtitleStreams(profile, container, fileName);
-                
+
+            BuildVideoStreams(profile, ref container, fileName);
+            BuildAudioStreams(profile, ref container, fileName);
+            BuildSubtitleStreams(profile, ref container, fileName);
+
             BaseImage sprite = new Sprite()
                 .SetScale(320)
                 .SetFilename("thumbs_:framesize:");
             container.AddStream(sprite);
-            
+
             var ffmpeg = new FfMpeg()
                 .Open(InputFile);
-        
+
             ffmpeg.SetBasePath(basePath);
             ffmpeg.SetTitle(title);
             ffmpeg.ToFile(fileName);
-        
+
             ffmpeg.AddContainer(container);
-        
+
             ffmpeg.Build();
-        
+
             var fullCommand = ffmpeg.GetFullCommand();
             Logger.Encoder(fullCommand);
-        
+
             var progressMeta = new ProgressMeta
             {
                 Id = movie?.Id ?? episode!.Id,
@@ -104,73 +103,77 @@ public class EncodeVideoJob : AbstractEncoderJob
                     x.VideoCodec.Value == VideoCodecs.H264Nvenc.Value || x.VideoCodec.Value == VideoCodecs.H265Nvenc.Value),
                 IsHDR = container.VideoStreams.Any(x => x.IsHdr),
             };
-        
-            // var result = await ffmpeg.Run(fullCommand, basePath, progressMeta);
-            // Logger.Encoder(result);
-        
+
+            var result = await ffmpeg.Run(fullCommand, basePath, progressMeta);
+            Logger.Encoder(result);
+
             await sprite.BuildSprite(progressMeta);
-        
+
             container.BuildMasterPlaylist();
         }
     }
 
-    private void BuildVideoStreams(EncoderProfile profile, BaseContainer container)
+    private void BuildVideoStreams(EncoderProfile encoderProfile, ref BaseContainer container, string fileName)
     {
-        // var stream = new X264(VideoCodecs.H264Nvenc.Value)
-        //     .SetScale(FrameSizes._4k.Width, -2)
-        //     .SetConstantRateFactor(20)
-        //     .ConvertHdrToSdr()
-        //     .SetHlsSegmentFilename(":type:_:framesize:_SDR/:type:_:framesize:_SDR")
-        //     .SetHlsPlaylistFilename(":type:_:framesize:_SDR/:type:_:framesize:_SDR")
-        //     .SetColorSpace(ColorSpaces.Yuv420p)
-        //     .SetPreset(VideoPresets.Fast)
-        //     .SetTune(VideoTunes.Hq)
-        //     .AddOpts("no-scenecut")
-        //     .AddOpts("keyint=48")
-        //     .AddCustomArgument("-x264opts", "no-scenecut");
-        //
-        // container.AddStream(stream);
-        
-        var stream2 = new X264(VideoCodecs.H264Nvenc.Value)
-            .SetScale(FrameSizes._1080p.Width)
-            .SetConstantRateFactor(20)
-            .ConvertHdrToSdr()
-            .SetHlsSegmentFilename(":type:_:framesize:_SDR/:type:_:framesize:_SDR")
-            .SetHlsPlaylistFilename(":type:_:framesize:_SDR/:type:_:framesize:_SDR")
-            .SetColorSpace(ColorSpaces.Yuv420p)
-            .SetPreset(VideoPresets.Fast)
-            .SetTune(VideoTunes.Hq)
-            .AddOpts("no-scenecut")
-            .AddOpts("keyint", 48)
-            .AddCustomArgument("-x264opts", "no-scenecut");
-        
-        container.AddStream(stream2);
-    }
+        foreach (var profile in encoderProfile.VideoProfiles)
+        {
+            BaseVideo stream = BaseVideo.Create(profile.Codec)
+                .SetScale(profile.Width, profile.Height) // FrameSizes._1080p.Width
+                .SetConstantRateFactor(profile.Crf) //20
+                .SetFrameRate(profile.Framerate) //24
+                .SetKiloBitrate(profile.Bitrate) // 5000
+                .ConvertHdrToSdr()
+                .SetHlsSegmentFilename(profile.SegmentName) //":type:_:framesize:_SDR/:type:_:framesize:_SDR"
+                .SetHlsPlaylistFilename(profile.PlaylistName) //":type:_:framesize:_SDR/:type:_:framesize:_SDR"
+                .SetColorSpace(profile.ColorSpace) //ColorSpaces.Yuv420p
+                .SetPreset(profile.Preset) //VideoPresets.Fast
+                .SetTune(profile.Tune) //VideoTunes.Hq
+                .AddOpts("keyint", profile.Keyint);  //"keyint", 48
 
-    private void BuildAudioStreams(EncoderProfile profile, BaseContainer container)
-    {
-        BaseAudio stream = new Aac()
-            .SetAudioChannels(2)
-            .SetAllowedLanguages([
-                Languages.Dut, Languages.Eng, Languages.Jpn, Languages.Fre, Languages.Ger, Languages.Ita, Languages.Spa,
-                Languages.Por, Languages.Rus, Languages.Kor, Languages.Chi, Languages.Ara
-            ])
-            .SetHlsSegmentFilename(":type:_:language:_:codec:/:type:_:language:_:codec:")
-            .SetHlsPlaylistFilename(":type:_:language:_:codec:/:type:_:language:_:codec:");
-        
-        container.AddStream(stream);
-    }
+            foreach (var opt in profile.Opts)
+            {
+                stream.AddOpts(opt); //"no-scenecut"
+            }
 
-    private void BuildSubtitleStreams(EncoderProfile profile, BaseContainer container, string fileName)
-    {
-            BaseSubtitle stream = new Vtt()
-                .SetAllowedLanguages([
-                    Languages.Dut, Languages.Eng, Languages.Jpn, Languages.Fre, Languages.Ger, Languages.Ita, Languages.Spa,
-                    Languages.Por, Languages.Rus, Languages.Kor, Languages.Chi, Languages.Ara
-                ])
-                .SetHlsSegmentFilename(":type:_:language:_:codec:/_:language:_:codec:")
-                .SetHlsPlaylistFilename($"subtitles/{fileName}.:language:.:variant:");
-            
+            foreach (var (key, val) in profile.CustomArguments)
+            {
+                stream.AddCustomArgument(key, val); //"-x264opts", "no-scenecut"
+            }
+
             container.AddStream(stream);
+        }
+    }
+
+    private void BuildAudioStreams(EncoderProfile encoderProfile, ref BaseContainer container, string fileName)
+    {
+        foreach (var profile in encoderProfile.AudioProfiles)
+        {
+            BaseAudio stream = BaseAudio.Create(profile.Codec)
+                .SetAudioChannels(profile.Channels) // 2
+                .SetAllowedLanguages(profile.AllowedLanguages) //[
+                //     Languages.Dut, Languages.Eng, Languages.Jpn, Languages.Fre, Languages.Ger, Languages.Ita,
+                //     Languages.Spa,  Languages.Por, Languages.Rus, Languages.Kor, Languages.Chi, Languages.Ara
+                // ])
+                .SetHlsSegmentFilename(profile.SegmentName) //":type:_:language:_:codec:/:type:_:language:_:codec:"
+                .SetHlsPlaylistFilename(profile.PlaylistName); //":type:_:language:_:codec:/:type:_:language:_:codec:"
+
+            container.AddStream(stream);
+        }
+    }
+
+    private void BuildSubtitleStreams(EncoderProfile encoderProfile, ref BaseContainer container, string fileName)
+    {
+        foreach (var profile in encoderProfile.SubtitleProfiles)
+        {
+            BaseSubtitle stream = BaseSubtitle.Create(profile.Codec)
+                .SetAllowedLanguages(profile.AllowedLanguages) //[
+                //     Languages.Dut, Languages.Eng, Languages.Jpn, Languages.Fre, Languages.Ger, Languages.Ita,
+                //     Languages.Spa,  Languages.Por, Languages.Rus, Languages.Kor, Languages.Chi, Languages.Ara
+                // ])
+                .SetHlsSegmentFilename(profile.SegmentName) //":type:_:language:_:codec:/_:language:_:codec:"
+                .SetHlsPlaylistFilename(profile.PlaylistName); //$"subtitles/{fileName}.:language:.:variant:";
+
+            container.AddStream(stream);
+        }
     }
 }
