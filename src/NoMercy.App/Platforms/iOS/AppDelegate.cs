8623f30a2fb3bf9fe80c6c75a8e9d0ceabb55cec
Stoney_Eagle <45034970+StoneyEagle@users.noreply.github.com>
2025-04-04 20:37:24 +0200

    refactor: remove unused resources and files from NoMercy.App

--- src/NoMercy.App/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace NoMercy.App;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
